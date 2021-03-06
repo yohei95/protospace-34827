34e2c8b8aef0ba0daefd8452556022aab028f9a5
yohei95 <yuhi0002@gmail.com>
2021-03-06 14:23:00 +0900

    PrototypesController befiore action修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user! , only: [:new, :edit, :destroy] 
-  before_action :move_to_index, only: [:edit, :new]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes=Prototype.all
